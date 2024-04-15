fb6ec3559904687aba7eec3d748dba78c4c900a2
Stuart Pearson <stuart.pearson@philips.com>
2024-04-15 21:14:58 +0200

    Added setup

--- custom_components/havanmoof/__init__.py
+++ custom_components/havanmoof/__init__.py
@@ -1,1 +1,9 @@
 """VanMoof Home Assistant"""
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+async def async_setup(
+    hass: HomeAssistant, config: Config
+):  # pylint: disable=unused-argument;
+    """Setting up this integration using YAML is not supported."""
+    return True
