ef2ddafed10deb9284b3409f3ca0de6e49437063
Stuart Pearson <stuart.pearson@philips.com>
2024-04-15 21:33:42 +0200

    Added basics

--- custom_components/havanmoof/__init__.py
+++ custom_components/havanmoof/__init__.py
@@ -1,9 +1,29 @@
-"""VanMoof Home Assistant"""
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-async def async_setup(
-    hass: HomeAssistant, config: ConfigEntry
-):  # pylint: disable=unused-argument;
+"""VanMoof Home Assistant"""
+
+DOMAIN = "havanmoof"
+
+async def async_setup(hass: HomeAssistant, config: ConfigEntry):
     """Setting up this integration using YAML is not supported."""
     return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up this integration using UI."""
+    if hass.data.get(DOMAIN) is None:
+        _LOGGER.info("Starting up VanMoof integration")
+
+    # Perform any additional setup here
+
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    # Perform any additional cleanup here
+    return True
+
+async def async_remove_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Remove a config entry."""
+    # Perform any additional cleanup here
+    return True 
