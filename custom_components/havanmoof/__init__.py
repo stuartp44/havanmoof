c93505c53b4205787b2fdde469d9120103b50026
Stuart Pearson <stuart.pearson@philips.com>
2024-04-15 21:21:23 +0200

    Change class import

--- custom_components/havanmoof/__init__.py
+++ custom_components/havanmoof/__init__.py
@@ -3,7 +3,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
 async def async_setup(
-    hass: HomeAssistant, config: Config
+    hass: HomeAssistant, config: ConfigEntry
 ):  # pylint: disable=unused-argument;
     """Setting up this integration using YAML is not supported."""
     return True
