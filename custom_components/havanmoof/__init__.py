fa350867ceb7726ba46042abfd8596f0462bdd8f
Stuart Pearson <1926002+stuartp44@users.noreply.github.com>
2024-04-19 13:16:08 +0200

    Added model

--- custom_components/havanmoof/__init__.py
+++ custom_components/havanmoof/__init__.py
@@ -9,10 +9,10 @@ DOMAIN = "havanmoof"
 _LOGGER = logging.getLogger(__name__)
 
 # Define the VanMoof UUIDs
-VANMOOF_UUIDS = {
-    "6acc5540-e631-4069-944d-b8ca7598ad50",
-    "8e7f1a50-087a-44c9-b292-a2c628fdd9aa",
-    "6acb5520-e631-4069-944d-b8ca7598ad50",
+vanmoof_bike_uuids = {
+    "SX3":"6acc5540-e631-4069-944d-b8ca7598ad50",
+    "SX1/SX2":"8e7f1a50-087a-44c9-b292-a2c628fdd9aa",
+    "SX1":"6acb5520-e631-4069-944d-b8ca7598ad50",
 }
 
 async def async_setup(hass: HomeAssistant, config: ConfigEntry):
@@ -44,14 +44,15 @@ async def process_discovered_service_info(hass):
     except Exception as e:
         _LOGGER.error("Error processing discovered service info: %s", e)
 
-def process_discovered_service_info_in_executor(hass):
+def process_discovered_service_info_in_executor(hass, vanmoof_bike_uuids):
     """Process discovered Bluetooth service info in a separate thread."""
     try:
         service_infos = hass.components.bluetooth.async_discovered_service_info(hass, connectable=True)
         for service_info in service_infos:
-            # Check if any of the service UUIDs are in the VanMoof UUIDs set
-            if any(uuid in VANMOOF_UUIDS for uuid in service_info.service_uuids):
-                _LOGGER.info("Discovered VanMoof service: UUIDs=%s, Address=%s, RSSI=%s",
-                             service_info.service_uuids, service_info.address, service_info.rssi)
+            # Check if any of the service UUIDs are in the VanMoof UUIDs map
+            for model, uuid in vanmoof_bike_uuids.items():
+                if uuid in service_info.service_uuids:
+                    _LOGGER.info("Discovered VanMoof Bike: Model=%s, UUIDs=%s, Address=%s, RSSI=%s",
+                                model, service_info.service_uuids, service_info.address, service_info.rssi)
     except Exception as e:
         _LOGGER.error("Error processing discovered service info: %s", e)
