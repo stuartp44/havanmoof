8030a655726a2b1a3aa3bc9d1ef4f2ee622f6f41
Stuart Pearson <1926002+stuartp44@users.noreply.github.com>
2024-06-23 20:04:30 +0200

    Update __init__.py

--- custom_components/havanmoof/__init__.py
+++ custom_components/havanmoof/__init__.py
@@ -1,86 +1,29 @@
-"""VanMoof Bike Home Assistant"""
-from datetime import timedelta
+"""The Vanmoof Bike integration."""
+from __future__ import annotations
+
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.exceptions import ConfigEntryNotReady
-from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
-from pymoof.tools import retrieve_bikes
-from homeassistant.const import (
-    Platform,
-)
-import logging
-from .const import DOMAIN, CONF_SCAN_INTERVAL
 
-_LOGGER = logging.getLogger(__name__)
-PLATFORMS: list[Platform] = [Platform.SENSOR, Platform.BINARY_SENSOR]
+from .const import DOMAIN
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Set up this integration using UI."""
+PLATFORMS: list[Platform] = [Platform.SENSOR]
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Vanmoof Bike from a config entry."""
+
     hass.data.setdefault(DOMAIN, {})
 
-    coordinator = VanMoofCoordinator(hass, entry)
-
-    await coordinator.async_refresh()
-    
-    if not coordinator.last_update_success:
-        raise ConfigEntryNotReady
-
-    hass.data[DOMAIN][entry.entry_id] = coordinator
-    
+    # Forward the setup to the sensor platform
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-    
-    if hass.data.get(DOMAIN) is None:
-        _LOGGER.info("Starting up VanMoof integration")
 
     return True
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    # Perform any additional cleanup here
-    return True
+    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
+        hass.data[DOMAIN].pop(entry.entry_id)
 
-async def async_remove_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Remove a config entry."""
-    # Perform any additional cleanup here
-    return True
-        
-class VanMoofCoordinator(DataUpdateCoordinator):
-    """Data update coordinator."""
-
-    def __init__(self, hass: HomeAssistant, entry: ConfigEntry) -> None:
-        """Initialize the coordinator."""
-        super().__init__(
-            hass,
-            _LOGGER,
-            name=DOMAIN,
-            update_interval=timedelta(
-                minutes=CONF_SCAN_INTERVAL
-            ),
-            username=entry.data.get("username"),
-            password=entry.data.get("password"),
-        )
-        self._entry = entry
-
-
-    @property
-    def entry_id(self) -> str:
-        """Return entry ID."""
-        return self._entry.entry_id
-    
-    # define username and password as properties
-    @property
-    def username(self) -> str:
-        """Return username."""
-        return self._entry.data.get("username")
-    
-    @property
-    def password(self) -> str:
-        """Return password."""
-        return self._entry.data.get("password")
-    
-
-    async def _async_get_bikes(self) -> dict:
-        """Fetch the latest data from the source."""
-        bikes = await self.hass.async_add_executor_job(retrieve_bikes.query, self.username, self.password)
-        return bikes
+    return unload_ok
