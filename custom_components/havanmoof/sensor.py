85d24294f467add7eaacc08ae0fa375f1a993556
Stuart Pearson <1926002+stuartp44@users.noreply.github.com>
2024-06-23 20:02:40 +0200

    Update sensor.py

--- custom_components/havanmoof/sensor.py
+++ custom_components/havanmoof/sensor.py
@@ -1,97 +1,102 @@
-"""Platform for typical sensor integration."""
+import logging
+
+from pymoof.tools import retrieve_bikes, discover_bike
+
+from homeassistant.components import bluetooth
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import Entity
-from dataclasses import dataclass
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
 from .const import DOMAIN
-from homeassistant.components.sensor import (
-    SensorDeviceClass,
-    SensorEntity,
-    SensorStateClass,
-)
 
-@dataclass
-class VanMoofEntityDescription(SensorEntityDescription):
-    """Provide a description of a Polar sensor."""
+_LOGGER = logging.getLogger(__name__)
 
-    key_category: str | None = None
-    unique_id: str | None = None
-    attributes_keys: list[str] | None = None
 
-SENSORS = (
-    VanMoofEntityDescription(
-        key_category=ATTR_USER_DATA,
-        key="weight",
-        name="Weight",
-        unique_id="weight",
-        native_unit_of_measurement="kg",
-        device_class=SensorDeviceClass.WEIGHT,
-        state_class=SensorStateClass.MEASUREMENT,
-    ),
-)
 async def async_setup_entry(
     hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
-) -> None:
-    """Set up the Polar sensor platform."""
-    coordinator: PolarCoordinator = hass.data[DOMAIN][entry.entry_id]
-    async_add_entities(BikeBatterySensor(coordinator, description) for description in SENSORS)
+):
+    """Set up the Vanmoof sensors from a config entry."""
 
-class BikeBatterySensor(CoordinatorEntity[PolarCoordinator], SensorEntity):
-    """Implementation of the Polar sensor."""
-
-    entity_description: PolarEntityDescription
-    _attr_attribution = ATTRIBUTION
-    _attr_has_entity_name = True
-
-    def __init__(
-        self,
-        coordinator: PolarCoordinator,
-        description: PolarEntityDescription,
-    ) -> None:
-        """Initialize the sensor."""
-        super().__init__(coordinator)
-        self.entity_description = description
-
-        self._attr_device_info = DeviceInfo(
-            configuration_url="https://flow.polar.com/",
-            entry_type=DeviceEntryType.SERVICE,
-            identifiers={(DOMAIN, coordinator.entry_id)},
-            manufacturer="Polar",
-            name=coordinator.user_name,
+    # Get bikes from the Vanmoof API
+    try:
+        bikes = await hass.async_add_executor_job(
+            retrieve_bikes.query, entry.data["username"], entry.data["password"]
         )
-        self._attr_unique_id = (
-            f"{coordinator.entry_id}_{description.unique_id or description.key}"
-        )
+    except Exception as e:
+        _LOGGER.error("Error setting up Vanmoof platform for sensor: %s", e)
+        return False
+
+    _LOGGER.info("Found %s bikes on VanMoof API", len(bikes))
+
+    sensors = []
+    for bike in bikes:
+        sensors.append(VanmoofBatterySensor(bike, entry))
+        sensors.append(VanmoofPresenceSensor(bike, entry))
+
+    async_add_entities(sensors)
+
+    # Get all advertisted bikes using bluetooth
+    try:
+        bluetooth_descovered_bikes = await check_and_discover_bikes(hass)
+    except Exception as e:
+        _LOGGER.error("Error setting up Vanmoof platform for sensor: %s", e)
+
+    if bluetooth_descovered_bikes:
+        _LOGGER.info("Found %s bikes using bluetooth", len(bluetooth_descovered_bikes))
+    else:
+        _LOGGER.info("No bikes found using bluetooth")
+
+async def check_and_discover_bikes(hass):
+    """Check and discover bikes using Bluetooth."""
+    scanner = bluetooth.async_get_scanner(hass)
+    discovered_bikes = await discover_bike.query(scanner)
+    return discovered_bikes
+
+class VanmoofBatterySensor(Entity):
+    def __init__(self, bike, entry):
+        self._bike = bike
+        self._entry = entry
+        self._attr_name = f"{bike['name']} Battery"
+        self._attr_unique_id = f"{bike['id']}_battery"
+        self._attr_unit_of_measurement = "%"
+        self._attr_device_class = "battery"
 
     @property
-    def available(self) -> bool:
-        """Return True if entity is available."""
-
-        return (
-            super().available
-            and self.entity_description.key
-            in self.coordinator.data[self.entity_description.key_category]
-        )
+    def state(self):
+        return "100"
 
     @property
-    def native_value(self) -> float | None:
-        """Return sensor state."""
-        if (
-            value := self.coordinator.data[self.entity_description.key_category][
-                self.entity_description.key
-            ]
-        ) is None:
-            return None
-        return value
+    def device_info(self):
+        return {
+            "identifiers": {(DOMAIN, self._bike["id"])},
+            "name": self._bike["name"],
+            "manufacturer": "VanMoof",
+            "model": self._bike["modelDetails"]["Edition"],
+            "sw_version": self._bike["smartmoduleCurrentVersion"],
+            "serial_number": self._bike["frameNumber"],
+        }
+
+
+class VanmoofPresenceSensor(Entity):
+    def __init__(self, bike, entry):
+        self._bike = bike
+        self._entry = entry
+        self._attr_name = f"{bike['name']} Presence"
+        self._attr_unique_id = f"{bike['id']}_presence"
+        self._attr_device_class = "presence"
 
     @property
-    def extra_state_attributes(self) -> Mapping[str, Any] | None:
-        """Return attributes."""
-        if self.entity_description.attributes_keys:
-            attributes = {}
-            for key in self.entity_description.attributes_keys:
-                if key in self.coordinator.data[self.entity_description.key_category]:
-                    value = self.coordinator.data[self.entity_description.key_category][
-                        key
-                    ]
-                    attributes.update({key: value})
-            return attributes
-        return None
+    def state(self):
+        return True
+
+    @property
+    def device_info(self):
+        return {
+            "identifiers": {(DOMAIN, self._bike["id"])},
+            "name": self._bike["name"],
+            "manufacturer": "VanMoof",
+            "model": self._bike["modelDetails"]["Edition"],
+            "sw_version": self._bike["smartmoduleCurrentVersion"],
+            "serial_number": self._bike["frameNumber"],
+        }
